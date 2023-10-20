e70a5919df5f2260f940e0552e723244354c43e8
taxe10 <tanchavez@lbl.gov>
2023-10-20 14:07:48 -0700

    updated comments in code

--- src/helper_utils.py
+++ src/helper_utils.py
@@ -26,29 +26,39 @@ def split_dataset(dataset, val_pct):
     return train_set, val_set
 
 
-def get_dataloaders(data, #splash_uri,
-                    batch_size, num_workers, shuffle=False, target_size=None,
-                    horz_flip_prob=0, vert_flip_prob=0, brightness=0, contrast=0, saturation=0, hue=0,
-                    val_pct=None):
+def get_dataloaders(data, batch_size, num_workers, shuffle=False, target_size=None, horz_flip_prob=0,
+                    vert_flip_prob=0, brightness=0, contrast=0, saturation=0, hue=0, val_pct=None):
     '''
     This function creates the dataloaders in PyTorch from directory or npy files
     Args:
-        splash_uri:     [str] Splash URI to Tagging Event UID for tracking purposes
+        data:           [str] File path to data details
         batch_size:     [int] Batch size
         num_workers:    [int] Number of workers
         shuffle:        [bool] Shuffle data
         target_size:    [tuple] Target size
         horz_flip_prob: [float] Probability of horizontal flip
         vert_flip_prob: [float] Probability of vertical flip
-        brightness:     [float]
-        contrast:
-        saturation:
-        hue:
+        ############################################################################################
+        Following descriptions were taken from https://pytorch.org/vision/main/generated/torchvision.transforms.ColorJitter.html
+        brightness:     [float, non negative] How much to jitter brightness, brightness_factor is
+                        chosen uniformly from [max(0, 1 - brightness), 1 + brightness]
+        contrast:       [float, non negative] How much to jitter contrast, contrast_factor is chosen 
+                        uniformly from [max(0, 1 - contrast), 1 + contrast]
+        saturation:     [float, non negative] How much to jitter saturation. saturation_factor is 
+                        chosen uniformly from [max(0, 1 - saturation), 1 + saturation].
+        hue:            [float] How much to jitter hue, hue_factor is chosen uniformly from 
+                        [-hue, hue]. Should have 0<= hue <= 0.5 or -0.5 <= min <= max <= 0.5. 
+                        To jitter hue, the pixel values of the input image has to be non-negative 
+                        for conversion to HSV space; thus it does not work if you normalize your 
+                        image to an interval with negative values, or use an interpolation that 
+                        generates negative values before using this function.
+        ############################################################################################
         val_pct:        [int] Percentage for validation [0-100]
     Returns:
         PyTorch DataLoaders
+        Image size, e.g. (input_channels, width, height)
+        List of data URIs
     '''
-
     # Definition of data transforms
     data_transform = []
     if brightness>0 or contrast>0 or saturation>0 or hue>0:
@@ -57,18 +67,20 @@ def get_dataloaders(data, #splash_uri,
         data_transform.append(transforms.RandomHorizontalFlip(p=horz_flip_prob))
     if vert_flip_prob>0:
         data_transform.append(transforms.RandomVerticalFlip(p=vert_flip_prob))
-
+    # Resize if needed
     if target_size:
         data_transform.append(transforms.Resize(target_size))
     data_transform.append(transforms.ToTensor())
+    # Load data information
     data_info = pd.read_parquet(data, engine='pyarrow')
     if 'local_uri' in data_info:
-        dataset = CustomDirectoryDataset(data_info['local_uri'], transforms.Compose(data_transform))
+        dataset = CustomDirectoryDataset(data_info['local_uri'],
+                                         transforms.Compose(data_transform))
     else:
-        dataset = CustomDirectoryDataset(data_info['uri'], transforms.Compose(data_transform))
+        dataset = CustomDirectoryDataset(data_info['uri'],
+                                         transforms.Compose(data_transform))
     (input_channels, width, height) = dataset[0][0].shape
     datasets_uris = data_info['uri']
-
     if val_pct:
         train_set, val_set = split_dataset(dataset, val_pct)
         train_loader = torch.utils.data.DataLoader(
@@ -105,7 +117,7 @@ def embed_imgs(model, data_loader):
         Latent space representation of the data
     '''
     embed_list = []
-    for counter, imgs in enumerate(data_loader):
+    for imgs in data_loader:
         with torch.no_grad():
             z = model.encoder(imgs[0].to(model.device))
         embed_list.append(z)
