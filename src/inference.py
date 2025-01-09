a630267929f8db3348a6982e63a14dd84a5f201e
Tanny Chavez <tanchavez@lbl.gov>
2025-01-08 22:21:48 -0800

    Rename and organize training, tunning, and inference scripts

--- src/inference.py
+++ src/inference.py
@@ -11,9 +11,10 @@ import torch
 import yaml
 from PIL import Image
 
-from helper_utils import embed_imgs, get_dataloaders, write_results
+from dataloaders import get_inference_dataloaders
+from helper_utils import embed_imgs, write_results
+from model import Autoencoder
 from parameters import InferenceParameters, IOParameters
-from src.model import Autoencoder, Decoder, Encoder  # noqa: F401
 
 warnings.filterwarnings("ignore")
 logger = logging.getLogger("pytorch_lightning")
@@ -27,14 +28,17 @@ if __name__ == "__main__":
     with open(args.yaml_path, "r") as file:
         parameters = yaml.safe_load(file)
 
+    # Parse parameters
     io_parameters = IOParameters.parse_obj(parameters["io_parameters"])
-    inference_parameters = InferenceParameters.parse_obj(parameters)
+    inference_parameters = InferenceParameters.parse_obj(parameters["model_parameters"])
 
+    # Set device
     device = (
         torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
     )
     logger.info("Device:" + str(device))
 
+    # Set target size
     if inference_parameters.target_width * inference_parameters.target_height > 0:
         target_size = (
             inference_parameters.target_width,
@@ -43,22 +47,25 @@ if __name__ == "__main__":
     else:
         target_size = None
 
-    # Define dataloaders
-    inference_loader, (temp_channels, temp_w, temp_h) = get_dataloaders(
+    # Get dataloaders
+    inference_loader, (temp_channels, temp_w, temp_h) = get_inference_dataloaders(
         io_parameters.data_uris,
         io_parameters.root_uri,
         io_parameters.data_type,
         inference_parameters.batch_size,
         inference_parameters.num_workers,
-        shuffle=False,
         target_size=target_size,
         log=inference_parameters.log,
-        train=False,
-        api_key=io_parameters.data_tiled_api_key,
-        detector_name=inference_parameters.detector_name,
+        data_tiled_api_key=io_parameters.data_tiled_api_key,
+        detector_uri=io_parameters.detector_uri,
+        detector_source=io_parameters.detector_source,
+        detector_tiled_api_key=io_parameters.detector_tiled_api_key,
     )
 
-    model = Autoencoder.load_from_checkpoint(io_parameters.model_dir)
+    # Load model
+    model = Autoencoder.load_from_checkpoint(
+        f"{io_parameters.model_dir}/{io_parameters.uid_retrieve}/last.ckpt"
+    )
 
     # Get latent space representation of inference images and reconstructed images
     start = time.time()
@@ -66,15 +73,14 @@ if __name__ == "__main__":
     logger.info(f"Time taken to embed images: {time.time() - start:.2f} seconds")
 
     # Create output directory if it does not exist
-    output_dir = Path(f"{io_parameters.output_dir}/{io_parameters.uid_save}")
-    output_dir.mkdir(parents=True, exist_ok=True)
+    results_dir = Path(f"{io_parameters.results_dir}/{io_parameters.uid_save}")
+    results_dir.mkdir(parents=True, exist_ok=True)
 
     # Save latent space representation of inference images
     df = pd.DataFrame(inference_img_embeds.cpu().detach().numpy())
     df.columns = df.columns.astype(str)
-    df.to_parquet(f"{output_dir}/f_vectors.parquet", engine="pyarrow")
+    df.to_parquet(f"{results_dir}/f_vectors.parquet", engine="pyarrow")
 
-    write_results(df, io_parameters, f"{output_dir}/f_vectors.parquet", parameters)
     logger.info("Latent space representation saved")
 
     # Reconstructed images
@@ -86,13 +92,28 @@ if __name__ == "__main__":
         colormode = "RGB"
     else:
         colormode = "L"
+        inference_result = np.squeeze(inference_result, axis=-1)
 
-    start = time.time()
     # Save reconstructed images
-    for indx in range(inference_result.shape[0]):
+    start = time.time()
+    recons_size = inference_result.shape
+    recons_filepaths = []
+    for indx in range(recons_size[0]):
+        filepath = f"{results_dir}/reconstructed_{indx}.jpg"
         im = Image.fromarray(
             (np.squeeze(inference_result[indx]) * 255).astype(np.uint8)
         )
         im = im.convert(colormode)
-        im.save(f"{output_dir}/reconstructed_{indx}.jpg")
+        im.save(filepath)
+        recons_filepaths.append(filepath)
     logger.info(f"Reconstructed images saved in {time.time() - start:.2f} seconds")
+
+    # Write results to Tiled
+    write_results(
+        df,
+        io_parameters,
+        f"{results_dir}/f_vectors.parquet",
+        inference_result,
+        parameters,
+    )
+    logger.info("Results written to tiled")
